6bd6c8bfcb3f3ecd3e7b5400b70f538b5e9c0a16
wangjinglong <arsenal591@126.com>
2020-08-23 18:59:14 +0800

    Basic main script.

---
+++ export_to_markdown.py
@@ -0,0 +1,12 @@
+import argparse
+
+from controller import Controller
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--token_v2', required=True, help='Your notion.so page\'s token, which can be obtained by inspecting browser cookies.')
+parser.add_argument('--page', required=True, help='The notion page\'s id or URL.')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    ctl = Controller(token_v2=args.token_v2)
+    ctl.get_serializer(ctl.notion_cli.get_block(args.page)).write()
